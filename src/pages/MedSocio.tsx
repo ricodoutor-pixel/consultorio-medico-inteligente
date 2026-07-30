d87c50b69b9f65cd208550ae034f755bb98f811d
ricodoutor-pixel <ricodoutor@gmail.com>
2026-07-30 11:07:27 -0300

    otimizacao da pagina
    telemed

--- src/pages/MedSocio.tsx
+++ src/pages/MedSocio.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";

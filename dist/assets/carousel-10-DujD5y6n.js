ade902956281c4fc12e631af63479e378e425892
ricodoutor-pixel <ricodoutor@gmail.com>
2026-08-09 02:40:38 -0300

    comits
    comits

---
+++ dist/assets/carousel-10-DujD5y6n.js
@@ -0,0 +1,1 @@
+const s="/assets/carousel-10-Bo8yD4ev.jpg";export{s as default};
---
+++ dist/assets/label-HAqH9H8A.js
@@ -0,0 +1,1 @@
+import{r,j as t,aY as i}from"./vendor-CwWG2DsU.js";import{N as a}from"./ui-primitives-ChqdtZcu.js";import{i as m}from"./index-CjmZrdVa.js";const l=i("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),p=r.forwardRef(({className:s,...e},o)=>t.jsx(a,{ref:o,className:m(l(),s),...e}));p.displayName=a.displayName;export{p as L};
